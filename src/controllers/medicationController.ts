22efa162f5fd25432356e458f8fb30ebda21c280
Eugenio Matias Brave <eugenio_m_brave@hotmail.com>
2025-10-13 12:34:51 -0300

    harcodeo de info vademecum para efront

--- src/controllers/medicationController.ts
+++ src/controllers/medicationController.ts
@@ -4,10 +4,35 @@ export class MedicationController {
   // GET /api/medications - Obtener todos los medicamentos
   static async getAllMedications(req: Request, res: Response) {
     try {
-      // Aquí implementarías la lógica para obtener medicamentos de la base de datos
+      // Simular la obtención de medicamentos desde una base de datos
+
+      // Responder con la lista de medicamentos
+
       const medications = [
-        { id: 1, name: "Paracetamol", category: "Analgésico" },
-        { id: 2, name: "Ibuprofeno", category: "Antiinflamatorio" },
+        {
+          id: 1,
+          name: "Metformina",
+          category: "Antidiabético",
+          description:
+            "Usada para mejorar el control del azúcar en sangre en personas con diabetes tipo 2.",
+          drug: "Metformina 500mg",
+        },
+        {
+          id: 2,
+          name: "Insulina Humana",
+          category: "Hormona",
+          description:
+            "Usada para controlar los niveles de azúcar en sangre en personas con diabetes tipo 2.",
+          drug: "Insulina Humana 100 UI/mL",
+        },
+        {
+          id: 3,
+          name: "Insulina Glargina",
+          category: "Hormona",
+          description:
+            "Usada para controlar los niveles de azúcar en sangre en personas con diabetes.",
+          drug: "Insulina Glargina 100 UI/mL",
+        },
       ];
 
       res.status(200).json({
